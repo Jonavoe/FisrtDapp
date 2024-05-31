ee98facafd087d6fa8bf7e3038f53ecc7ce74bed
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-05-30 21:36:10 -0300

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,50 +1,45 @@
 "use client";
 
-// import { useState } from "react";
-// import { AddressInput } from "~~/components/scaffold-eth";
-// import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/iconoshit.png";
+import { motion } from "framer-motion";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  // const [address, setAddress] = useState("");
-  // const { data: actualOwner } = useScaffoldReadContract({
-  //   contractName: "YourContract",
-  //   functionName: "owner",
-  // });
-
-  // const { writeContractAsync: setNewOwner } = useScaffoldWriteContract("YourContract");
-
-  // const setNewOwnerHandler = async () => {
-  //   await setNewOwner({
-  //     functionName: "setNewOwner",
-  //     args: [address],
-  //   });
-  // };
-
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
-        {/* <div className="card w-[30rem] bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Set New Owner</h2>
-            <label htmlFor="">Actual Owner</label>
-            <AddressInput
-              value={actualOwner || ""}
-              disabled={true}
-              placeholder="Current Owner"
-              onChange={e => {
-                console.log("Disabled input changed:", e);
-              }}
-            />
-            <label htmlFor="">New Owner</label>
-            <AddressInput onChange={setAddress} value={address} placeholder="Input your address" />
-            <div className="card-actions justify-end mt-2">
-              <button className="btn btn-primary w-full" onClick={() => setNewOwnerHandler()}>
-                Set New Owner
-              </button>
-            </div>
-          </div>
-        </div> */}
+        <motion.div
+          initial={{ y: -30, scale: 0, opacity: 0 }}
+          animate={{ y: 0, scale: 1, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 2 }}
+        >
+          <Image src={logo} alt="Logo" />
+        </motion.div>
+        <motion.div
+          initial={{ x: 80, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 2 }}
+        >
+          <h1 className="text-5xl">Welcome to the New Era of Harry Coin</h1>
+        </motion.div>
+        <motion.div
+          initial={{ x: -80, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 2 }}
+        >
+          <p className="text-xl">On the next page, you can purchase HRY tokens to explore our innovative features.</p>
+        </motion.div>
+        <motion.div
+          initial={{ y: 80, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 2 }}
+        >
+          <Link href={"/harry"} passHref>
+            <button className="btn btn-outline w-36 text-xl">Buy HRY</button>
+          </Link>
+        </motion.div>
       </div>
     </>
   );
